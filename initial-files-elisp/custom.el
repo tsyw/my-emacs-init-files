4d5ace5196a13613a76b23e2d91af696d9be1ec5
tsyw <1028472974@qq.com>
2017-01-20 17:19:16 +0800

    change company-delay

--- initial-files-elisp/custom.el
+++ initial-files-elisp/custom.el
@@ -4,18 +4,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.08)
- '(company-minimum-prefix-length 1)
- '(custom-buffer-indent 4)
- '(indent-tabs-mode nil)
- '(inhibit-startup-screen t)
- '(initial-scratch-message nil)
  '(LaTeX-mode-hook
    (quote
     (LaTeX-math-mode outline-minor-mode turn-on-reftex turn-on-cdlatex)))
  '(TeX-PDF-mode nil)
  '(TeX-fold-auto t)
  '(add-hook (quote LaTeX-mode-hook) t)
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 3)
+ '(custom-buffer-indent 4)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil)
+ '(org-src-fontify-natively t)
  '(preview-image-type (quote png))
  '(reftex-allow-automatic-rescan t)
  '(reftex-enable-partial-scans t)
