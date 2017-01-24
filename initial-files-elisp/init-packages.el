0a2b2d154366251dbf9dc37ec7df398a7825a427
tsyw <1028472974@qq.com>
2017-01-25 00:23:27 +0800

    change smartparens for lisp-interaction-mode

--- initial-files-elisp/init-packages.el
+++ initial-files-elisp/init-packages.el
@@ -66,6 +66,7 @@
 ;;smartparens
 (smartparens-global-mode t)
 (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+(sp-local-pair 'lisp-interaction-mode "'" nil :actions nil)
 
 ;;popwin
 (require 'popwin)
